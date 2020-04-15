db5fe52b0b0dd99a9a7f2657dce19259d253f14b
abc7468 <abc7468@naver.com>
2020-04-16 03:15:33 +0900

    N과 M

---
+++ Algoritms/beginning/15654.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+
+int N, M;
+int map[9];
+int tmp[9];
+int visit[9];
+
+void back(int count) {
+	if (count == M) {
+		for (int i = 0; i < M; i++) {
+			cout << tmp[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+
+		if (!visit[i]) {
+			tmp[count] = map[i];
+			count++;
+			visit[i] = true;
+			back(count);
+			count--;
+			visit[i] = false;
+		}
+	}
+
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		cin >> map[i];
+	}
+
+	sort(map, map + N);
+	back(0);
+
+
+}
---
+++ Algoritms/beginning/15666.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<set>
+using namespace std;
+
+
+int N, M;
+int map[9];
+int tmp[9];
+int check[9];
+int visit[9];
+vector<int> value;
+set<vector<int>> s;
+
+
+
+void back(int start, int count) {
+	if (count == M) {
+		for (int i = 0; i < M; i++) {
+			s.insert(value);
+		}
+		return;
+	}
+	for (int i = start; i < N; i++) {
+		count++;
+		value.push_back(map[i]);
+		back(i, count);
+		value.pop_back();
+		count--;
+	}
+}
+
+set<vector<int>>::iterator iter;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		cin >> map[i];
+	}
+
+	sort(map, map + N);
+	back(0, 0);
+
+	for (auto k : s) {
+		for (auto t : k) printf("%d ", t);
+		printf("\n");
+	}
+
+
+}
